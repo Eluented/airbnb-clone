f46e07382a5b6ae8fe38ad12be1e3f989ca6a0fc
Onur <onurbelek@outlook.com>
2025-02-23 14:24:07 +0000

    structure of login page set with css

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,12 +1,83 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { useWarmUpBrowser } from "@/hooks/useWarmUpBrowser";
+import { defaultStyles } from "@/constants/Styles";
+import Colors from "@/constants/Colors";
 
 const Page = () => {
+  useWarmUpBrowser();
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      <TextInput
+        autoCapitalize="none"
+        placeholder="Email"
+        style={[defaultStyles.inputField, { marginBottom: 30 }]}
+      />
 
-export default Page
+      <TouchableOpacity style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </TouchableOpacity>
+
+      <View style={styles.seperatorView}>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: "black",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+        <Text style={styles.seperator}>or</Text>
+        <View
+          style={{
+            flex: 1,
+            borderBottomColor: "black",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 26,
+  },
+
+  seperatorView: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+    marginVertical: 30,
+  },
+  seperator: {
+    fontFamily: "mon-sb",
+    color: Colors.grey,
+    fontSize: 16,
+  },
+  btnOutline: {
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: Colors.grey,
+    height: 50,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    paddingHorizontal: 10,
+  },
+  btnOutlineText: {
+    color: "#000",
+    fontSize: 16,
+    fontFamily: "mon-sb",
+  },
+});
+export default Page;
