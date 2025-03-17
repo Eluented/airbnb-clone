5d33619cf0453399e80017868e7cde0411960301
Onur <onurbelek@outlook.com>
2025-03-17 15:32:41 +0000

    changed config

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,6 +1,15 @@
-import { View, Text, FlatList, ListRenderItem } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  ListRenderItem,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from "react-native";
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 interface Props {
   listings: any[];
@@ -19,10 +28,41 @@ const Listings = ({ listings: items, category }: Props) => {
       setLoading(false);
     }, 200);
   }, [category]);
-  
 
   const renderRow: ListRenderItem<any> = ({ item }) => (
-    <Link href={`/listing/${item.id}`}>Go there</Link>
+    <Link href={`/listing/${item.id}`} asChild>
+      <TouchableOpacity>
+        <View style={styles.listing}>
+          <Image source={{ uri: item.medium_url }} />
+          <TouchableOpacity
+            style={{ position: "absolute", right: 30, top: 30 }}
+          >
+            <Ionicons name="heart-outline" size={24} color="#000" />
+          </TouchableOpacity>
+
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text style={{ fontSize: 16, fontFamily: "mon-sb" }}>
+              {item.name}
+            </Text>
+            <View style={{ flexDirection: "row", gap: 4 }}>
+              <Ionicons name="star" size={16} />
+              <Text style={{ fontFamily: "mon-sb" }}>
+                {item.review_scores_rating / 20}
+              </Text>
+            </View>
+          </View>
+
+          <Text style={{ fontFamily: "mon" }}>{item.room_type}</Text>
+
+          <View style={{ flexDirection: 'row', gap: 4}}>
+            <Text style={{ fontFamily: 'mon-sb'}}>$ {item.price}</Text>
+            <Text style={{fontFamily: 'mon'}}>night</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </Link>
   );
   return (
     <View>
@@ -34,5 +74,24 @@ const Listings = ({ listings: items, category }: Props) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  listing: {
+    padding: 16,
+    gap: 10,
+    marginVertical: 16,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+    borderRadius: 10,
+  },
+  info: {
+    textAlign: "center",
+    fontFamily: "mon-sb",
+    fontSize: 16,
+    marginTop: 4,
+  },
+});
 
 export default Listings;
