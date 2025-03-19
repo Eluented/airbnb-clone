fd0efb5716b8884e7eed339194e6a1e4e3017693
Onur <onurbelek@outlook.com>
2025-03-19 19:29:02 +0000

    maps added in ListingsMap - locations through listings.geo.json

--- components/ListingsMap.tsx
+++ components/ListingsMap.tsx
@@ -1,14 +1,44 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
-import MapView from "react-native-maps";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { Platform, PermissionsAndroid, Alert } from "react-native";
+import Geolocation from "@react-native-community/geolocation";
+import { ListingGeo } from "@/interfaces/listingGeo";
+
+const INITIAL_REGION = {
+  latitude: 37.33,
+  longitude: -122,
+  latitudeDelta: 9,
+  longitudeDelta: 9,
+};
 
 interface Props {
   listings: any;
 }
 export default function ListingsMap({ listings }: Props) {
+  const onMarkerSelected = (event: any) => {
+    console.log(event);
+  };
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        style={StyleSheet.absoluteFill}
+        showsUserLocation
+        showsMyLocationButton
+        provider={PROVIDER_GOOGLE}
+        initialRegion={INITIAL_REGION}
+      >
+        {listings.features.map((item: ListingGeo) => (
+          <Marker
+            key={item.properties.id}
+            onPress={() => onMarkerSelected}
+            coordinate={{
+              latitude: +item.properties.latitude,
+              longitude: +item.properties.longitude,
+            }}
+          />
+        ))}
+      </MapView>
     </View>
   );
 }
